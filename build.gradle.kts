c22c8cd03f7466f4a61d890f4a902d43f20fc207
Hacreon <pandeypriyanshu2343@gmail.com>
2024-09-09 14:50:43 +0530

    The Workout App with history and BMI calculator

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+
+
+plugins {
+    id("com.android.application") version "8.6.0" apply false
+    id("com.android.library") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
+}
